e2118ed522b5813d085a5438332772b7e680d0e9
Levy Nunes <levy.vix@gmail.com>
2024-02-08 23:28:27 -0300

    Add files via upload

--- perguntas_negocio.sql
+++ perguntas_negocio.sql
@@ -2,12 +2,12 @@
 select
 m.machine_id ,
 m2.model,
-count(*)
+count(*) as failure_count
 from  final.failures f 
 left join "final".machine m  on f.machineid  = m.machine_id 
 left join final.models m2 on m.model_id = m2.model_id 
 group  by m.machine_id, m2.model 
-order by count(*) desc
+order by failure_count desc
 limit 1
  
 
@@ -22,14 +22,45 @@ order by f_count desc
 
 
 -- Qual componente apresenta maior quantidade de falhas por máquina.
+
 select
-c.component_name,
-count(*) as f_count
-from final.failures f 
+distinct 
+m.machine_id ,
+c.component_name ,
+count(*) as failure_count
+from "final".failures f 
 left join final.components c on f.component_key = c.component_key 
-group by c.component_name 
-order by f_count desc
-limit 1
+left join final.machine m on f.machineid  = m.machine_id 
+group by m.machine_id, c.component_name 
+order by failure_count desc
+
+
+
+WITH ranked AS (
+    SELECT
+        m.machine_id,
+        c.component_name,
+        COUNT(*) AS failure_count,
+        ROW_NUMBER() OVER (PARTITION BY m.machine_id ORDER BY COUNT(*) DESC) AS rank
+    FROM
+        final.failures f
+    LEFT JOIN
+        final.components c ON f.component_key = c.component_key
+    LEFT JOIN
+        final.machine m ON f.machineid = m.machine_id
+    GROUP BY
+        m.machine_id,
+        c.component_name
+)
+SELECT
+    machine_id,
+    component_name,
+    failure_count
+FROM
+    ranked
+where rank = 1
+ORDER BY
+    machine_id;
 
 -- A média da idade das máquinas por modelo
 select
